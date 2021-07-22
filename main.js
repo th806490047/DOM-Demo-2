058e1a966f46e1def5d04e99d7cc8d96ba9520d7
th806490047 <806490047@163.com>
2021-07-22 15:53:17 +0800

    第一版DOM封装作业

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const div = dom.find('#test>.red')[0]
+dom.style(div, 'color', 'red')
+console.log("___________________________________________")
+
+const divList = dom.find('.red') // 获取多个 div.red 元素
+dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素
